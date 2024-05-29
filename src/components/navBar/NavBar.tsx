3dccdf6253494cc84163239e5ac8d5fe536c5404
jennizs <jennyfrn6104@gmail.com>
2024-05-29 13:02:08 -0300

    Implementando_Rotas

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Basket } from "@phosphor-icons/react"
+import { Link } from "react-router-dom"
 
 
 
@@ -10,12 +11,13 @@ function Navbar() {
 
     return (
         <>
-            <div className='w-full  bg-[#ff5151] text-white flex justify-center py-4'>
+            <div className='w-full  bg-[#1d1f6d] text-white flex justify-center py-4'>
                 <div className="container flex justify-between text-lg">
                     <div className='text-2xl font-bold uppercase'>Farmazen.com</div>
                     <div className='flex gap-4'>
-                        <div className='hover:underline'>Categorias</div>
-                        <div className='hover:underline'>Nova Categoria</div>
+                    <Link to="/home" className='hover:underline'>Home</Link>
+                        <Link to="/categorias" className='hover:underline'>Categorias</Link>
+                        <Link to="/cadastrar-categoria" className='hover:underline'>Cadastrar-categoria</Link>
                         <div className='underline-basket'>
                             <Basket size={25} weight='bold' />
                         </div>
