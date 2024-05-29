25957860ec1f66a8f83c02df0c7118cc01925fe5
jennizs <jennyfrn6104@gmail.com>
2024-05-29 13:17:24 -0300

    implementando_rotas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
         <>
             <div className="flex justify-center bg-[#1d1f6d] text-white">
                 <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'> JennizsGames | Copyright: </p>
+                    <p className='text-xl font-bold'> Farmazen | Copyright: </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
                         <LinkedinLogo size={48} weight='bold' />
